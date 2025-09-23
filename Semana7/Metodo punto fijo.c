9697a60bdd05e0a8b63dc8d99ab7c4e2671cb9cd
Rubi200415 <pr.hernandezfloreano@ugto.mx>
2025-09-23 00:12:57 -0600

    Add files via upload

---
+++ Semana7/Metodo punto fijo.c
@@ -0,0 +1,60 @@
+//Paola Rubi Hernández Floreano "METODO PUNTO FIJO"
+
+
+#include <stdio.h>
+#include <math.h>
+
+float f(float x) {
+    return exp(-x) - x;
+}
+
+float g_punto_fijo(float x) {
+    return exp(-x);
+}
+
+int main() {
+    float x0 = 0.0;
+    float tol = 0.000001;
+    float raiz, raiz_ant;
+    int cont = 0;
+    float err_rel, err_int;
+
+    printf("=== METODO PUNTO FIJO ===\n");
+    printf("g(x) = e^(-x)\n");
+    printf("x0 = %.1f\n", x0);
+    printf("Margen error: %f\n\n", tol);
+
+    printf("Iter | Raiz Ant | Raiz Act | Error Rel | Error Iter\n");
+    printf("---------------------------------------------------\n");
+    
+    raiz_ant = x0;
+    raiz = g_punto_fijo(x0);
+    cont = 1;
+    
+    err_rel = fabs((raiz - raiz_ant) / raiz);
+    err_int = fabs(raiz - raiz_ant);
+    
+    printf("%4d | %8.6f | %8.6f | %9.7f | %10.8f\n", 
+           cont, raiz_ant, raiz, err_rel, err_int);
+    
+    while (fabs(f(raiz)) > tol) {
+        cont++;
+        raiz_ant = raiz;
+        raiz = g_punto_fijo(raiz);
+        
+        err_rel = fabs((raiz - raiz_ant) / raiz);
+        err_int = fabs(raiz - raiz_ant);
+        
+        printf("%4d | %8.6f | %8.6f | %9.7f | %10.8f\n", 
+               cont, raiz_ant, raiz, err_rel, err_int);
+        
+        if (cont > 50) break;
+    }
+
+    printf("\nResultado final: %.6f\n", raiz);
+    printf("Valor funcion: %.8f\n", f(raiz));
+    printf("Total iteraciones: %d\n", cont);
+    printf("Ultimo error: %.8f\n", err_int);
+
+    return 0;
+}
